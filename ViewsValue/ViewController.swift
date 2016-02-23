e7c3f7b3835e7ac45acc55a00650d2ae8bf17fdf
taiduosummer <542596009@qq.com>
2016-02-23 13:25:13 +0800

    2.23

--- ViewsValue/ViewController.swift
+++ ViewsValue/ViewController.swift
@@ -15,9 +15,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let backVeiw = UIView(frame: CGRectMake(100,300,200,200))
+        view.addSubview(backVeiw)
+        
+        let pathBe = UIBezierPath(arcCenter: CGPointMake(100, 100), radius: 80, startAngle: CGFloat(-M_PI/2), endAngle: CGFloat(-(2*M_PI + M_PI/2)), clockwise: false)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor.redColor().CGColor
+        shapeLayer.lineDashPattern = [6,3]
+        shapeLayer.fillColor = UIColor.whiteColor().CGColor
+        shapeLayer.lineJoin = kCALineJoinRound
+        shapeLayer.path = pathBe.CGPath
+        backVeiw.layer.addSublayer(shapeLayer)
+
+        
+        
+        let animaitonz = CABasicAnimation(keyPath: "transform.rotation.z")
+        animaitonz.duration = 2
+        animaitonz.fromValue = 0
+        animaitonz.toValue = -M_PI*3/2
+        backVeiw.layer.addAnimation(animaitonz, forKey: "rotaiton")
+    
+        
+        let animaitonEnd = CABasicAnimation(keyPath: "strokeEnd")
+        animaitonEnd.fillMode = kCAFillModeForwards
+        animaitonEnd.fromValue = 0.25
+        animaitonEnd.toValue = 1
+        animaitonEnd.duration = 2
+        shapeLayer.addAnimation(animaitonEnd, forKey: "end")
         
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -37,6 +65,22 @@ class ViewController: UIViewController {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
