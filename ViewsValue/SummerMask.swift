f02727a1cb934d2e7cd72e587c0c0d4ab1408ccc
taiduosummer <542596009@qq.com>
2016-02-25 17:06:43 +0800

    2.25  CASpringAnimation   保持动画后状态

--- ViewsValue/SummerMask.swift
+++ ViewsValue/SummerMask.swift
@@ -15,31 +15,31 @@ class MaskLayer {
         //        viewMask.backgroundColor = UIColor.blueColor()
 //        view.addSubview(viewMask)
         
-        let leftLayer = CAGradientLayer()
-        leftLayer.frame = CGRectMake(0, 0, 100, 200)
-        leftLayer.colors = [UIColor.greenColor().CGColor,UIColor.blueColor().CGColor]
+        let leftLayer        = CAGradientLayer()
+        leftLayer.frame      = CGRectMake(0, 0, 100, 200)
+        leftLayer.colors     = [UIColor.greenColor().CGColor,UIColor.blueColor().CGColor]
         leftLayer.startPoint = CGPointMake(0, 1)
-        leftLayer.endPoint = CGPointMake(1, 0)
+        leftLayer.endPoint   = CGPointMake(1, 0)
         viewMask.layer.addSublayer(leftLayer)
         
-        let rightLayer = CAGradientLayer()
-        rightLayer.frame = CGRectMake(100, 0, 100, 200)
-        rightLayer.colors = [UIColor.blueColor().CGColor,UIColor.redColor().CGColor]
+        let rightLayer        = CAGradientLayer()
+        rightLayer.frame      = CGRectMake(100, 0, 100, 200)
+        rightLayer.colors     = [UIColor.blueColor().CGColor,UIColor.redColor().CGColor]
         rightLayer.startPoint = CGPointMake(0, 0)
-        rightLayer.endPoint = CGPointMake(1, 1)
+        rightLayer.endPoint   = CGPointMake(1, 1)
         viewMask.layer.addSublayer(rightLayer)
         
         
-        let pathLayer = CAShapeLayer()
+        let pathLayer       = CAShapeLayer()
         pathLayer.lineWidth = 10.0
-        pathLayer.lineJoin = kCALineJoinRound
-        pathLayer.lineCap = kCALineCapRound
+        pathLayer.lineJoin  = kCALineJoinRound
+        pathLayer.lineCap   = kCALineCapRound
         
-        pathLayer.fillColor = UIColor.clearColor().CGColor
+        pathLayer.fillColor   = UIColor.clearColor().CGColor
         pathLayer.strokeColor = UIColor.whiteColor().CGColor
-        let pathBese = UIBezierPath(arcCenter: CGPointMake(100, 100), radius: 80, startAngle: CGFloat(M_PI*1.5/2), endAngle: CGFloat(2.25 * M_PI), clockwise: true)
-        pathLayer.path = pathBese.CGPath
-        viewMask.layer.mask = pathLayer
+        let pathBese          = UIBezierPath(arcCenter: CGPointMake(100, 100), radius: 80, startAngle: CGFloat(M_PI*1.5/2), endAngle: CGFloat(2.25 * M_PI), clockwise: true)
+        pathLayer.path        = pathBese.CGPath
+        viewMask.layer.mask   = pathLayer
         //        viewMask.layer.masksToBounds = true
         
         pathLayer.strokeEnd = 1
--- ViewsValue/ViewController.swift
+++ ViewsValue/ViewController.swift
@@ -15,48 +15,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let viewMask = UIView(frame: CGRectMake(100,200,200,100))
-        //        viewMask.backgroundColor = UIColor.blueColor()
-        view.addSubview(viewMask)
+        let label = CALayer()
+        label.frame = CGRect(x: 100, y: 200, width: 30, height: 30)
+        label.backgroundColor = UIColor.redColor().CGColor
+        view.layer.addSublayer(label)
+    
         
-        let leftLayer = CAGradientLayer()
-        leftLayer.frame = CGRectMake(0, 0, 100, 200)
-        leftLayer.colors = [UIColor.greenColor().CGColor,UIColor.blueColor().CGColor]
-        leftLayer.startPoint = CGPointMake(0, 1)
-        leftLayer.endPoint = CGPointMake(1, 0)
-        viewMask.layer.addSublayer(leftLayer)
-        
-        let rightLayer = CAGradientLayer()
-        rightLayer.frame = CGRectMake(100, 0, 100, 200)
-        rightLayer.colors = [UIColor.blueColor().CGColor,UIColor.redColor().CGColor]
-        rightLayer.startPoint = CGPointMake(0, 0)
-        rightLayer.endPoint = CGPointMake(1, 1)
-        viewMask.layer.addSublayer(rightLayer)
-        
-        
-        let pathLayer = CAShapeLayer()
-        pathLayer.lineWidth = 10.0
-        pathLayer.lineJoin = kCALineJoinRound
-        pathLayer.lineCap = kCALineCapRound
-        
-        pathLayer.fillColor = UIColor.clearColor().CGColor
-        pathLayer.strokeColor = UIColor.whiteColor().CGColor
-        let pathBese = UIBezierPath(arcCenter: CGPointMake(100, 100), radius: 80, startAngle: CGFloat(M_PI*1.5/2), endAngle: CGFloat(2.25 * M_PI), clockwise: true)
-        pathLayer.path = pathBese.CGPath
-        viewMask.layer.mask = pathLayer
-//        viewMask.layer.masksToBounds = true
-        
-        pathLayer.strokeEnd = 1
-        pathLayer.strokeStart = 0
-        
-        let animaitonEnd = CABasicAnimation(keyPath: "strokeEnd")
-        animaitonEnd.fillMode = kCAFillModeForwards
-        animaitonEnd.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        animaitonEnd.fromValue = 0
-        animaitonEnd.toValue = 1
-        animaitonEnd.duration = 2
-        pathLayer.addAnimation(animaitonEnd, forKey: "end")
-        
+        let spring = CASpringAnimation(keyPath: "position.x")
+        spring.damping = 5;
+        spring.stiffness = 100;
+        spring.mass = 1;
+        spring.initialVelocity = 10;
+        spring.fromValue = label.position.x;
+        spring.toValue = label.position.x + 50;
+        spring.duration = spring.settlingDuration;
+        spring.fillMode = kCAFillModeForwards//1
+        spring.removedOnCompletion = false//2保持动画后状态
+        label.addAnimation(spring, forKey: spring.keyPath);
     }
     
     
